da8ff99e555cdfe4c12dc2948b244c081083cd33
Stanislav Evseev <stasevseev@gmail.com>
2017-02-15 11:31:10 +0300

    Changed loading image to using asyncio. Initialization during running.

--- storage/apps.py
+++ storage/apps.py
@@ -11,8 +11,8 @@ class StorageConfig(AppConfig):
 
     def ready(self):
         t0 = time.time()
-        self.storage = StorageCSVRecords(url=DOCSHEETS_URL)
-        # self.storage.init_cache()
+        self.storage = AsyncStorageCSVRecords(url=DOCSHEETS_URL)
+        self.storage.init_cache()
         print("Boot times", time.time() - t0)
 
     def get_storage(self):
